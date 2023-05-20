f22631111c9bbf76fa7feeccea1c54f86a7d3090
Ben Song <40846869+bensong04@users.noreply.github.com>
2023-05-19 19:37:54 -0700

    Update img_to_qr.py

--- img_to_qr.py
+++ img_to_qr.py
@@ -6,7 +6,6 @@ if __name__ == '__main__':
     digitalObject = cv2.imread(sys.argv[1])
     fileType='jpeg'
     byteEncodedDigitalObject=imgToByte(digitalObject, fileType)
-    print(byteEncodedDigitalObject)
 
     digital_ID, byteEncodedDigitalID = modelv2(byteEncodedDigitalObject)
     QR(digital_ID).save(sys.argv[2])
